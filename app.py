f79b3e4d2fc497e0c3cafd893b382c5f529b9947
farisulhaq <faries089@gmail.com>
2023-12-08 18:43:35 +0700

    add dockerfile

--- app.py
+++ app.py
@@ -1,4 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template, request
+import pandas as pd
+import numpy as np
+import os
+import joblib
+from sklearn.metrics import adjusted_mutual_info_score
+from IPython.display import HTML
+import pickle
 
 app = Flask(__name__)
 
